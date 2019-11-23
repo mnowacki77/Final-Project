587046f946ea3cff8d19eb6820ca787109ebcd8e
mnowacki77 <mixil77@gmail.com>
2019-11-23 12:00:02 +0100

    @Entity

--- src/main/java/com/sda/tasklist/model/todo/ToDoEntity.java
+++ src/main/java/com/sda/tasklist/model/todo/ToDoEntity.java
@@ -4,13 +4,21 @@ import com.sda.tasklist.model.Category;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@Entity
 public class ToDoEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
     private String description;
     private Category category;
--- src/main/java/com/sda/tasklist/model/user/UserRoleEntity.java
+++ src/main/java/com/sda/tasklist/model/user/UserRoleEntity.java
@@ -1,26 +1,21 @@
 package com.sda.tasklist.model.user;
 
-import lombok.Getter;
+
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDate;
 
-@Getter
-@NoArgsConstructor
 @Entity
-public class UserEntity {
-
+@NoArgsConstructor
+@Data
+public class UserRoleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private String login;
-    private String email;
-    private String password;
-    private LocalDate birthDate;
-    private String securityQuestion;
+    private String name;
 }
