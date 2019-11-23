7a9b2aeaf407b6b210165622f35bbc442c96711c
mnowacki77 <mixil77@gmail.com>
2019-11-23 15:27:17 +0100

    Fixing date

--- src/main/java/com/sda/tasklist/dto/user/CreateUserForm.java
+++ src/main/java/com/sda/tasklist/dto/user/CreateUserForm.java
@@ -2,6 +2,7 @@ package com.sda.tasklist.dto.user;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -13,6 +14,7 @@ public class CreateUserForm {
     private String password;
     private String confirmedPassword;
     private String email;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
     private String securityQuestion;
     private String securityAnswer;
