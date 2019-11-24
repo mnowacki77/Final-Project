f6311c09aa0949f31992940ba8dbd56fb43b15fd
mnowacki77 <mixil77@gmail.com>
2019-11-24 12:01:43 +0100

    Front end: 1.has role Admin link in menu 2.headers in users.htm

--- src/main/java/com/sda/tasklist/TaskListApplication.java
+++ src/main/java/com/sda/tasklist/TaskListApplication.java
@@ -3,32 +3,50 @@ package com.sda.tasklist;
 import com.sda.tasklist.dao.user.UserRoleRepository;
 import com.sda.tasklist.model.user.UserRoleEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @SpringBootApplication
 public class TaskListApplication implements CommandLineRunner {
 
-	@Autowired
-	private UserRoleRepository userRoleRepository;
+    @Autowired
+    private UserRoleRepository userRoleRepository;
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskListApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaskListApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		if (!userRoleRepository.existsByName("ADMIN")) {
-			userRoleRepository.save(new UserRoleEntity("ADMIN"));
-		}
-		if (!userRoleRepository.existsByName("USER")) {
-			userRoleRepository.save(new UserRoleEntity("USER"));
-		}
-
-
+    @Override
+    public void run(String... args) throws Exception {
+        if (!userRoleRepository.existsByName("ADMIN")) {
+            userRoleRepository.save(new UserRoleEntity("ADMIN"));
+        }
+        if (!userRoleRepository.existsByName("USER")) {
+            userRoleRepository.save(new UserRoleEntity("USER"));
+        }
 
 
-	}
+    }
+
+    @Autowired
+    private ThymeleafProperties properties;
+    @Value("${spring.thymeleaf.templates_root:}")
+    private String templatesRoot;
+
+    @Bean
+    public ITemplateResolver defaultTemplateResolver() {
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setSuffix(properties.getSuffix());
+        resolver.setPrefix(templatesRoot);
+        resolver.setTemplateMode(properties.getMode());
+        resolver.setCacheable(properties.isCache());
+        return resolver;
+    }
 }
