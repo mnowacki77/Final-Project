cf970e598aa74442df0358a9276f15dc75b8edfc
krzlas <krzysiek.las@gmail.com>
2019-12-06 19:17:17 +0100

    Upgrade Webjars loading on all pages

--- src/main/java/com/sda/tasklist/TaskListApplication.java
+++ src/main/java/com/sda/tasklist/TaskListApplication.java
@@ -1,46 +1,40 @@
 package com.sda.tasklist;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
-import org.springframework.context.annotation.Bean;
-import org.thymeleaf.templateresolver.FileTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskListApplication implements CommandLineRunner {
+public class TaskListApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TaskListApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TaskListApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
 
-    }
-
-    @Autowired
-    private ThymeleafProperties properties;
-    @Value("${spring.thymeleaf.templates_root:}")
-    private String templatesRoot;
-
-    @Bean
-    public ITemplateResolver defaultTemplateResolver() {
-        FileTemplateResolver resolver = new FileTemplateResolver();
-        resolver.setSuffix(properties.getSuffix());
-        resolver.setPrefix(templatesRoot);
-        resolver.setTemplateMode(properties.getMode());
-        resolver.setCacheable(properties.isCache());
-        return resolver;
-    }
-
-    @Bean
-    public LayoutDialect layoutDialect() {
-        return new LayoutDialect();
-    }
+//    @Autowired
+//    private ThymeleafProperties properties;
+//    @Value("${spring.thymeleaf.templates_root:}")
+//    private String templatesRoot;
+//
+//    @Bean
+//    public ITemplateResolver defaultTemplateResolver() {
+//        FileTemplateResolver resolver = new FileTemplateResolver();
+//        resolver.setSuffix(properties.getSuffix());
+//        resolver.setPrefix(templatesRoot);
+//        resolver.setTemplateMode(properties.getMode());
+//        resolver.setCacheable(properties.isCache());
+//        return resolver;
+//    }
+//
+//    @Bean
+//    public LayoutDialect layoutDialect() {
+//        return new LayoutDialect();
+//    }
 }
