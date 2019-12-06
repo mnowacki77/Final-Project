831de790787293d1ad54ae07a1d21b093a05fc89
krzlas <krzysiek.las@gmail.com>
2019-12-06 21:28:03 +0100

    Upgrade navbar - now properly collapse
    Add sorting task by deadline

--- src/main/java/com/sda/tasklist/service/todo/ToDoServiceImpl.java
+++ src/main/java/com/sda/tasklist/service/todo/ToDoServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,7 +32,7 @@ public class ToDoServiceImpl implements ToDoService {
     @Override
     public List<ToDoDTO> getToDos() {
         UserEntity userEntity = userRepository.findByLogin(SecurityContextHolder.getContext().getAuthentication().getName()).get();
-        return userEntity.getToDoEntityList().stream().map(t -> ToDoMapper.map(t)).collect(Collectors.toList());
+        return userEntity.getToDoEntityList().stream().sorted(Comparator.comparing(ToDoEntity::getDeadline)).map(t -> ToDoMapper.map(t)).collect(Collectors.toList());
     }
 
     @Override
