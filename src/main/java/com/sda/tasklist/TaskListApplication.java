95eeb4810a9eb0e0b1c64e600991c376780652fa
krzlas <krzysiek.las@gmail.com>
2019-12-06 19:59:55 +0100

    Working version on localhost with "real-time" html pages swap.
    To work with external tomcat server:
    - change <scope>provided</scope> in pom
    - comment thymeleaf part in TaskListApplication

--- src/main/java/com/sda/tasklist/TaskListApplication.java
+++ src/main/java/com/sda/tasklist/TaskListApplication.java
@@ -1,9 +1,16 @@
 package com.sda.tasklist;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @SpringBootApplication
 public class TaskListApplication extends SpringBootServletInitializer {
@@ -18,23 +25,23 @@ public class TaskListApplication extends SpringBootServletInitializer {
     }
 
 
-//    @Autowired
-//    private ThymeleafProperties properties;
-//    @Value("${spring.thymeleaf.templates_root:}")
-//    private String templatesRoot;
-//
-//    @Bean
-//    public ITemplateResolver defaultTemplateResolver() {
-//        FileTemplateResolver resolver = new FileTemplateResolver();
-//        resolver.setSuffix(properties.getSuffix());
-//        resolver.setPrefix(templatesRoot);
-//        resolver.setTemplateMode(properties.getMode());
-//        resolver.setCacheable(properties.isCache());
-//        return resolver;
-//    }
-//
-//    @Bean
-//    public LayoutDialect layoutDialect() {
-//        return new LayoutDialect();
-//    }
+    @Autowired
+    private ThymeleafProperties properties;
+    @Value("${spring.thymeleaf.templates_root:}")
+    private String templatesRoot;
+
+    @Bean
+    public ITemplateResolver defaultTemplateResolver() {
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setSuffix(properties.getSuffix());
+        resolver.setPrefix(templatesRoot);
+        resolver.setTemplateMode(properties.getMode());
+        resolver.setCacheable(properties.isCache());
+        return resolver;
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
 }
